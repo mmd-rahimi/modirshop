1e9dfacf8be78b9be0dc52f47cca9e2e21a5a6f2
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-08-03 13:45:22 -0700

    added Hero

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,6 +20,7 @@ export default function RootLayout({
         className=""
       >
        <Header/>
+       <Hero/>
         {children}
         <Footer/>
       </body>
