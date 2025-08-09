31ee5fed31c326c421a5fad67f2dde59abee86d6
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-08-09 14:09:29 -0700

    little change on footer

--- apps/frontend/components/Footer.tsx
+++ apps/frontend/components/Footer.tsx
@@ -3,17 +3,15 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-
-        <div className="flex flex-row justify-center items-center text-white gap-4 text-xs sm:text-sm md:text-lg lg:text-xl">
-            <span>راه های ارتباطی</span>
-            <span>راه های ارتباطی</span>
-            <span>راه های ارتباطی</span>
+      <div className="flex flex-col justify-center items-center gap-3 w-full px-6">
+        <div className="flex flex-row justify-center items-center w-full text-white gap-6 text-xs sm:text-sm md:text-lg lg:text-xl">
+            <span>راه های ارتباطی:</span>
+            <span>linkedin</span>
+            <span>portfolio</span>
+            <span>gmail</span>
+            <span>phone</span>
         </div>
-
-      <div className="copyright-box">
-        <div className="container-footer">
-          <p>© {new Date().getFullYear()} Mohamad Mahdi Rahimi Dev</p>
-        </div>
+          <p className="text-gray-300">© {new Date().getFullYear()} Mohamad Mahdi Rahimi Dev</p>
       </div>
     </footer>
   );
