62e000b339286522a055df2ab7e46e3a5d2f756e
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-08-11 10:55:18 -0700

    orders handled on dashboard

--- apps/admin/app/orders/page.tsx
+++ apps/admin/app/orders/page.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import { enTofa } from "@/utils/Utilities";
 
-function page() {
-  return (
-    <div>page</div>
-  )
+interface CartItem {
+  title: string;
+  quantity: number;
 }
 
-export default page
+interface User {
+  name: string;
+  email: string;
+  country: string;
+  city: string;
+  postalCode: string;
+}
+
+interface Order {
+  _id: string;
+  user: User;
+  totalPrice: number;
+  status: string;
+  createAt: string;
+  cart: CartItem[];
+}
+
+async function Orders() {
+  const res = await fetch("http://localhost:3001/api/orders");
+  const orders: Order[] = await res.json();
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>نام کاربر</th>
+          <th>ایمیل</th>
+          <th>کشور - شهر - کدپستی</th>
+          <th>جمع کل</th>
+          <th>وضعیت</th>
+          <th>تاریخ</th>
+          <th>محصولات</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {orders.map((order) => (
+          <tr key={order._id}>
+            <td>{order.user.name}</td>
+            <td>{order.user.email}</td>
+            <td>
+              {order.user.country}-{order.user.city}-{order.user.postalCode}
+            </td>
+            <td>{`${enTofa(order.totalPrice)}  تومان`}</td>
+            <td>{order.status}</td>
+            <td>{new Date(order.createAt).toLocaleDateString("fa-IR")}</td>
+            <td>{order.cart.map((item) => item.title + item.quantity)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Orders;
